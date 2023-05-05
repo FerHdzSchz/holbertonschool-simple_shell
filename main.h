8ab4b204ae94d86e47f88935cd46cbdc29537c42
FerHdzSchz <fernandohdz915@hotmail.com>
2023-05-04 19:50:28 -0500

    betty main

--- main.h
+++ main.h
@@ -13,7 +13,7 @@
 
 
 int _putchar(char c);
-char* read_textfile(const char *filename, size_t letters);
+char *read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 char *_strncpy(char *dest, char *src, int n);
