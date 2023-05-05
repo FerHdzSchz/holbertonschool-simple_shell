a1435bbda54e33d546250ef74720e9a1ecdb7cae
FerHdzSchz <fernandohdz915@hotmail.com>
2023-05-05 14:09:13 -0500

    fixin betty style

--- executor.c
+++ executor.c
@@ -1,42 +1,45 @@
 #include "main.h"
 /**
- * executer
+ * executer - program to execute arguments
+ * @arguments: list of arguments
+ * @comp_path: complete path of first item in argument list
+ * @penv: pointer to pointer to environment variables
+ * Return:  Always 0
 */
 
 int executer(char **arguments, char *comp_path, char **penv)
 {
-    struct stat st;
-    pid_t my_pid;
+	pid_t my_pid;
 
-    my_pid = fork();
-    if (my_pid == -1)
-        exit(0);
-    else if (my_pid == 0)
-    {
-        if (execve(arguments[0], arguments, penv) == -1)
-        {
-            arguments[0] = malloc(sizeof(char) * _strlen(comp_path) + 1);
-            arguments[0] = comp_path;
-            if (execve(arguments[0], arguments, penv) == -1)
-            {
-                free(comp_path);
-                free(arguments[0]);
-                free(arguments);
-                exit(0);
-            }
-        }
-        free(comp_path);
-        free(arguments[0]);
-        free(arguments);
-        sleep(3);
-    }
-    else
-    {
-        waitpid(my_pid, NULL, 0);
-    }
-    free(comp_path);
-    free(arguments[0]);
-    free(arguments);
+	my_pid = fork();
+	if (my_pid == -1)
+		exit(0);
+	else if (my_pid == 0)
+	{
+		if (execve(arguments[0], arguments, penv) == -1)
+		{
+			arguments[0] = malloc(sizeof(char) * _strlen(comp_path) + 1);
+			arguments[0] = comp_path;
+			if (execve(arguments[0], arguments, penv) == -1)
+			{
+				free(comp_path);
+				free(arguments[0]);
+				free(arguments);
+				exit(0);
+			}
+		}
+		free(comp_path);
+		free(arguments[0]);
+		free(arguments);
+		sleep(3);
+	}
+	else
+	{
+		waitpid(my_pid, NULL, 0);
+	}
+	free(comp_path);
+	free(arguments[0]);
+	free(arguments);
 
-    return(1);
+	return (0);
 }
--- hsh.c
+++ hsh.c
@@ -12,7 +12,6 @@ int main(int ac, char **argv, char **env)
 	int line_chars;
 	char *buffer = NULL, *complete_path = NULL;
 	const char *separator = " \n";
-	pid_t my_pid;
 	struct stat st;
 
 	(void) ac;
@@ -27,44 +26,14 @@ int main(int ac, char **argv, char **env)
 			exit(0);
 		}
 		argv = parse_line(buffer, separator);
-
 		complete_path = _which(argv[0], env);
 		if (complete_path == NULL && stat(argv[0], &st) != 0)
-			{
+		{
 			free(buffer);
 			free(complete_path);
 			continue;
-			}
-		my_pid = fork();
-		if (my_pid == -1)
-			exit(0);
-		else if (my_pid == 0)
-		{
-			if (execve(argv[0], argv, env) == -1)
-			{
-				argv[0] = malloc(sizeof(char) * _strlen(complete_path) + 1);
-				argv[0] = complete_path;
-				if (execve(argv[0], argv, env) == -1)
-				{
-					free(complete_path);
-					free(argv[0]);
-					free(argv);
-					exit(0);
-				}
-			}
-			free(argv);
-			free(argv[0]);
-			free(complete_path);
-			free(buffer);
-			sleep(3);
 		}
-		else
-		{
-			waitpid(my_pid, NULL, 0);
-		}
-		free(complete_path);
-		free(argv[0]);
-		free(argv);
+		executer(argv, complete_path, env);
 	}
 	return (0);
 }
